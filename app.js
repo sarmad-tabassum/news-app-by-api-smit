c9126c9afde4a7ceff6d4fbafbff99903e3abece
Sarmad Tabassum <sarmadplaying@gmail.com>
2026-05-22 14:39:53 +0000

    'done'

--- app.js
+++ app.js
@@ -1,59 +1,89 @@
-let getDiv = document.getElementById("show");
+let show = document.getElementById("show");
+let loadMore = document.getElementById("loadMore");
+let nextPg = null;
 
-function searchNews() {
+function render(data) {
+  // console.log(data);
+  data.forEach((article) => {
+    let description = article.description || "No description available.";
+    if (description.length > 150) {
+      description = description.substring(0, 150) + "...";
+    }
+
+    show.innerHTML += `
+          <div class="abc">
+          <img src="${article.image_url || "https://techouse.com.pk/wp-content/themes/ryse/assets/images/no-image/No-Image-Found-400x264.png"}" />
+          <div class='news-content'>
+          <h2 class='news-title'>${article.title}</h2>
+          <p class="news-description">${description}</p>
+          <p class="author">${article.creator ? article.creator.join(", ") : "Unknown"}</p>
+          <p class="date">${article.pubDate ? new Date(article.pubDate).toLocaleString() : "No date"}</p>
+          <a class="read-more" href="${article.link}" target="_blank">Read More</a>
+          </div>
+          </div>
+          `;
+  });
+}
+
+loadMore.addEventListener("click", () => {
+  fetch(
+    `https://newsdata.io/api/1/news?apikey=pub_271291cdafdf43cda9cc5cf01f786f24&language=en&page=${nextPg}`,
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.results || data.results.length == 0) {
+        loadMore.style.display = "none";
+        return;
+      }
+
+      // console.log(data);
+      nextPg = data.nextPage || null;
+      render(data.results);
+    })
+
+    .catch((err) => {
+      console.error("Load more error:", err);
+    });
+});
+
+function searchNews(defaultQ = "tesla") {
   let getInp = document.getElementById("inp");
-  let query = getInp.value.trim(); // trim() start or end ki extra spaces khatam karta h
+  let query = getInp.value.trim() || defaultQ; // trim() start or end ki extra spaces khatam karta h
 
-  if (!query) {
-    alert("Please enter a keyword to search.");
-    return;
-  }
-
-  getDiv.innerHTML = "";
+  show.innerHTML = "";
+  loadMore.style.display = "block";
 
   fetch(
-    `https://newsdata.io/api/1/news?apikey=pub_77c17324d7e34f22b8244737585f02ac&q=${encodeURIComponent(query)}&language=en`,
+    `https://newsdata.io/api/1/news?apikey=pub_271291cdafdf43cda9cc5cf01f786f24&q=${query}&language=en`,
   )
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      //   console.log(data.results);
+      // console.log(data.nextPage);
       if (!data.results || data.results.length == 0) {
-        getDiv.innerHTML = "<p>No news found for this topic.</p>";
+        show.innerHTML = "<p>No news found for this topic.</p>";
         return;
       }
 
-      data.results.forEach((article) => {
-        console.log(article.pubDate);
-        
-        let description = article.description || "No description available.";
-        if (description.length > 150) {
-          description = description.substring(0, 150) + "...";
-        }
+    //   console.log(data);
 
-        getDiv.innerHTML += `
-    <div class="abc">
-        <img src="${article.image_url || "https://techouse.com.pk/wp-content/themes/ryse/assets/images/no-image/No-Image-Found-400x264.png"}" alt="news-img" />
-        <div class='news-content'>
-            <h2 class='news-title'>${article.title}</h2>
-            <p class="news-description">${description}</p>
-            <p class="author">${
-              article.creator ? article.creator.join(", ") : "Unknown Author"
-            }</p>
-            <p class="date">${
-              article.pubDate
-                ? new Date(article.pubDate).toLocaleString()
-                : "No date"
-            }</p>
-            <a class="read-more" href="${article.link}" target="_blank">Read More</a>
-        </div> 
-    </div>
-  `;
-      });
+      nextPg = data.nextPage;
+      render(data.results);
     })
     .catch((err) => {
       console.error("Fetch error:", err);
-      getDiv.innerHTML = "<p>Error fetching news. Try again later.</p>";
+      show.innerHTML = "<p>Error fetching news. Try again later.</p>";
     });
 }
+
+window.onload = () => {
+  searchNews();
+};
+
+document.getElementById("inp").addEventListener("keypress", (e) => {
+  // console.log(e.key == 'Enter');
+  if (e.key == "Enter") {
+    searchNews();
+  }
+});
